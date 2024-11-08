7d6693765197501c166ae9731b6d821976231502
mxrsel <marsel14082006@gmail.com>
2024-11-08 16:45:15 +0600

    installed axios and bootstrap and added axios api

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import {Provider} from "react-redux";
 import {store} from "./app/store.ts";
+import 'bootstrap/dist/css/bootstrap.css';
 
 createRoot(document.getElementById('root')!).render(
     <Provider store={store}>

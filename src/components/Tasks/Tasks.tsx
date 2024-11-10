a051bcd12d274770dfd366492d2406229122d013
mxrsel <marsel14082006@gmail.com>
2024-11-10 21:38:08 +0600

    created map for give data to every new created element, took tasks types from slices and rendering tasks by fetchTasks and useEffect

--- src/components/Tasks/Tasks.tsx
+++ src/components/Tasks/Tasks.tsx
@@ -1,8 +1,22 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { AppDispatch, RootState } from "../../app/store.ts";
+import { fetchTasks } from "./tasksSlice.ts";
+import TasksItem from "./TasksItem.tsx";
 
 const Tasks = () => {
+    const dispatch: AppDispatch = useDispatch();
+    const tasks = useSelector((state: RootState) => state.tasks.tasks);
+
+    useEffect(() => {
+        dispatch(fetchTasks());
+    }, [dispatch]);
+
     return (
         <div>
-
+            {tasks.map((task) => (
+                <TasksItem key={task.id} tasks={task} />
+            ))}
         </div>
     );
 };

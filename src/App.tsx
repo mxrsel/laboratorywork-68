ce21a64deae56c92d4060cd173049bb28cad7ba7
mxrsel <marsel14082006@gmail.com>
2024-11-10 21:38:31 +0600

    added component tasks to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import TodoForm from "./components/TodoForm/TodoForm";
+import Tasks from "./components/Tasks/Tasks.tsx";
 
 const App = () => {
 
 
     return (
         <div>
+            <Tasks />
             <TodoForm />
         </div>
     );

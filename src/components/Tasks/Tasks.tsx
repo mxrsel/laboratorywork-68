7742d8f3ac6ca076e74df71511a3d21591b2a6bc
mxrsel <marsel14082006@gmail.com>
2024-11-11 14:39:35 +0600

    added spinner and added it to tasks and added props from slice isLoading nad isError for display spinner and show errors

--- src/components/Tasks/Tasks.tsx
+++ src/components/Tasks/Tasks.tsx
@@ -3,21 +3,36 @@ import { useDispatch, useSelector } from "react-redux";
 import { AppDispatch, RootState } from "../../app/store.ts";
 import { fetchTasks } from "./tasksSlice.ts";
 import TasksItem from "./TasksItem.tsx";
+import Spinner from "../Spinner/Spinner.tsx";
 
 const Tasks = () => {
     const dispatch: AppDispatch = useDispatch();
     const tasks = useSelector((state: RootState) => state.tasks.tasks);
+    const isLoading = useSelector((state: RootState) => state.tasks.isLoading);
+    const isError = useSelector((state: RootState) => state.tasks.isError);
 
     useEffect(() => {
         dispatch(fetchTasks());
     }, [dispatch]);
 
+    if (isLoading) {
+        return <Spinner />
+    }
+
+    if (isError) {
+        return <p className='text-center mt-3 fs-2' >Error fetching tasks</p>
+    }
+
     return (
         <div>
             <h1 className='text-center mt-5'>Tasks List</h1>
-            {tasks.map((task) => (
-                <TasksItem key={task.id} tasks={task} />
-            ))}
+            {tasks.length === 0 ? (
+                <p>No tasks yet</p>
+            ) : (
+                tasks.map((task) => (
+                        <TasksItem key={task.id} tasks={task} />
+                    ))
+            )}
         </div>
     );
 };
